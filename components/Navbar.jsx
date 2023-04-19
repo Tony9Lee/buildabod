d07716ff8133ca2bf78dacd9a84fde1084066424
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-04-18 22:08:10 -0700

    BB - Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  return (
+    <Box
+      display="flex"
+      position="fixed"
+      top={0}
+      width="100%"
+      px={20}
+      bgcolor="#212121"
+      justifyContent="space-between"
+    >
+      <Typography fontSize={25} fontWeight={700} color="white">
+        BUILDABOD
+      </Typography>
+      <Box display="flex" alignItems="center" paddingRight={100}>
+        <Typography fontSize={18} color="white">
+          Item 1
+        </Typography>
+        <Typography fontSize={18} color="white" ml={3}>
+          Item 2
+        </Typography>
+        <Typography fontSize={18} color="white" ml={3}>
+          Item 3
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
