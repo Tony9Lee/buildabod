83652c9c6b9706489745b1446d5415fbc8fe3809
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-04-20 22:17:42 -0700

    BB - Interface

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Featured = () => {
   return (
-    <Box
-    px={20}
-      sx={{
-        borderRadius: "20px",
-        objectFit: "cover",
-        width: "100%",
-
-        height: 300,
-      }}
-    >
-      <img src="./public/g.webp"/>
+    <Box display="flex" direction="row" alignItems="flex-start">
+      <Stack
+        px={20}
+        marginLeft={-60}
+        sx={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <img src="./public/g.webp" style={{ borderRadius: "10px" }} />
+      </Stack>
+      <Box sx={{ paddingTop: "40px", width: "100%" }}>
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          BUILD YOUR BODY!
+        </Typography>
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          FIND YOUR SUSTENANCE!
+        </Typography>
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          BE THE HERO!
+        </Typography>
+      </Box>
     </Box>
-
   );
 };
 
