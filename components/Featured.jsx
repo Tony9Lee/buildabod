92b370df93314c8d2b64e73c983cb06cd27efc83
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-05-05 05:26:54 -0700

    update

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -27,42 +27,56 @@ const Featured = () => {
     videoRef.current.play();
   };
 
+  const handlePause = () => {
+    setIsPlaying(false);
+    videoRef.current.play();
+  };
+
   return (
-    <Box
-      position="relative"
-      display="flex"
-      direction="row"
-      alignItems="flex-start"
-    >
+<Box
+  position="relative"
+  display="flex"
+  direction="row"
+  alignItems="flex-start"
+>
+  <img
+    src="/background.jpg"
+    style={{ width: "100%", height: "100%", objectFit: "cover" }}
+  />
+  <Box position="absolute" bottom={500} left={20} right={0} px={2} py={4}>
+    {(!isPlaying && (
       <img
-        src="/background.jpg"
-        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        src="/cam1.jpg"
+        alt="Play button"
+        style={{ width: 200, height: 200, borderRadius: 115, cursor: "pointer" }}
+        onClick={handlePlay}
       />
-      <Box position="absolute" bottom={500} left={20} right={0} px={2} py={4}>
-        {!isPlaying && (
-          <img
-            src="/cam.png"
-            alt="Play button"
-            style={{ width: 200, height: 200, cursor: "pointer" }}
-            onClick={handlePlay}
-          />
-        )}
-        <video
-          ref={videoRef}
-          style={{
-            display: isPlaying ? "block" : "none",
-            position: "absolute",
-            left: "48%",
-            bottom: "-90%",
-            transform: "translate(-50%, -50%)",
-          }}
-          src="/video.mp4"
-          width="1200"
-          height="520"
-          controls
-        />
-      </Box>
-    </Box>
+    )) || (
+      <img
+        src="/cam2.jpg"
+        alt="Pause button"
+        style={{ width: 200, height: 200, borderRadius: 115, cursor: "pointer" }}
+        onClick={handlePause}
+      />
+    )}
+    <video
+      ref={videoRef}
+      style={{
+        display: isPlaying ? "block" : "none",
+        position: "absolute",
+        left: "48%",
+        bottom: "-22%",
+        transform: "translate(-50%, -50%)",
+      }}
+      src="/video.mp4"
+      width="1200"
+      height="520"
+      controls
+      onPlay={() => setIsPlaying(true)}
+      onPause={() => setIsPlaying(false)}
+    />
+  </Box>
+</Box>
   );
 };
 
