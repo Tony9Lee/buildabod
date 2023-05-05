7aae43c1d17aef95b997fc825d4efb6e7300b681
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-05-04 23:35:17 -0700

    Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Box, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { FaFilm, FaList, FaUser, FaSignInAlt } from "react-icons/fa";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
@@ -26,46 +28,71 @@ const Navbar = () => {
       display="flex"
       position={isHomePage ? "relative" : "absolute"}
       top={0}
-      width="100%"
+      width="70%"
       height={47}
       px={40}
       bgcolor="#212121"
       justifyContent="space-between"
     >
       <Typography fontSize={25} fontWeight={700} color="white">
-        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+        <NavLink to="/" style={{ textDecoration: "none", color: "white" }}>
           MOVIEREEL
-        </Link>
+        </NavLink>
       </Typography>
-      <Box display="flex" alignItems="center" ml={-55}>
-        <Typography fontSize={18} color="white">
-          <Link to="/movies" style={{ textDecoration: "none", color: "white" }}>
-            MOVIES
-          </Link>
-        </Typography>
-        <Typography fontSize={18} color="white" ml={3}>
-          <Link
-            to="/playlist"
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            PLAYLIST
-          </Link>
-        </Typography>
-        <Typography fontSize={18} color="white" ml={3}>
-          <Link
-            to="/profile"
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            PROFILE
-          </Link>
-        </Typography>
-        <Typography fontSize={18} color="white" ml={3}>
-          <Link to="/login" style={{ textDecoration: "none", color: "white" }}>
-            LOGIN
-          </Link>
-        </Typography>
+      <Box display="flex" alignItems="center" ml={3}>
+        <NavLink
+          to="/movies"
+          style={{
+            textDecoration: "none",
+            color: "white",
+            marginRight: 20,
+          }}
+          activeStyle={{
+            borderBottom: "2px solid white",
+          }}
+        >
+          <FaFilm size={18} />
+          <span>MOVIES</span>
+        </NavLink>
+        <NavLink
+          to="/playlist"
+          style={{
+            textDecoration: "none",
+            color: "white",
+            marginRight: 20,
+          }}
+          activeStyle={{
+            borderBottom: "2px solid white",
+          }}
+        >
+          <FaList size={18} />
+          <span style={{ marginLeft: 5 }}>PLAYLIST</span>
+        </NavLink>
+        <NavLink
+          to="/profile"
+          style={{
+            textDecoration: "none",
+            color: "white",
+            marginRight: 20,
+          }}
+          activeStyle={{
+            borderBottom: "2px solid white",
+          }}
+        >
+          <FaUser size={18} />
+          <span style={{ marginLeft: 5 }}>PROFILE</span>
+        </NavLink>
+        <NavLink
+          to="/login"
+          style={{ textDecoration: "none", color: "white" }}
+          activeStyle={{
+            borderBottom: "2px solid white",
+          }}
+        >
+          <FaSignInAlt size={18} />
+          <span style={{ marginLeft: 5 }}>LOGIN</span>
+        </NavLink>
       </Box>
-      <Box></Box>
     </Box>
   );
 };
