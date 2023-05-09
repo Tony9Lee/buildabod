6d4f22e4da97a3d2c801514e9a4c42aa3c5a0eed
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-05-09 16:53:44 -0700

    .

--- components/Reel.jsx
+++ components/Reel.jsx
@@ -1,58 +1,42 @@
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Box, Typography } from "@mui/material";
-import {
-  ArrowBackIosOutlined,
-  ArrowForwardIosOutlined,
-} from "@mui/icons-material";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Reel = ({ items, title }) => {
+import "swiper/css";
+import "swiper/css/pagination";
 
+import "./styles.css";
+
+import { Navigation } from "swiper";
+
+const Reel = () => {
   return (
-    <Box
-      sx={{
-        marginBottom: "20px",
-      }}
-    >
-      <Typography
-        variant="h5"
-        sx={{ marginLeft: "70px", marginBottom: "10px", color: "white", fontWeight: 700 }}
-      >
-        {title}
+    <>
+      <Typography marginBottom={3} color="red" fontSize={20} fontWeight={700}>
+        MOVIE TITLE
       </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          gap: "10px",
-          overflowX: "auto",
-          scrollBehavior: "smooth",
-          "&::-webkit-scrollbar": {
-            display: "none",
-          },
-        }}
-      
+      <Swiper
+        slidesPerView={5}
+        spaceBetween={20}
+        navigation
+        modules={[Navigation]}
+        className="mySwiper"
       >
-        <ArrowBackIosOutlined
-          sx={{ fontSize: "50px", color: "grey", cursor: "pointer" }}
-        />
-        {items.map((item, index) => (
-          <Box
-            key={index}
-            sx={{
-              width: "225px",
-              height: "120px",
-              backgroundColor: "white",
-            }}
-          >
-            <Typography>{item}</Typography>
-          </Box>
-        ))}
-        <ArrowForwardIosOutlined
-          sx={{ fontSize: "50px", color: "grey", cursor: "pointer" }}
-        />
-      </Box>
-    </Box>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 
